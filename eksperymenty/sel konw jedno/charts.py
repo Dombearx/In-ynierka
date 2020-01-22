a9172a9f9ffbc288683c26dbea0b22a6269a2650
Dombearx <domin.dober@gmail.com>
2020-01-22 21:18:33 +0100

    Duży commit

---
+++ eksperymenty/sel konw jedno/charts.py
@@ -0,0 +1,168 @@
+import utils
+import pickle
+import matplotlib.pyplot as plt
+import statistics
+import numpy as np
+import pprint as pp
+import math
+
+
+class IslandResults():
+
+    def __init__(self, min, avg, std, max, islandNumber):
+        self.min = min
+        self.avg = avg
+        self.std = std
+        self.max = max
+        self.islandNumber = islandNumber
+
+
+class Benchmark():
+
+    def __init__(self, numOfIslands):
+        self.benchmarkAverangeMax = []
+        self.benchmarkAverangeMin = []
+        self.benchmarkAverangeAvg = []
+        self.benchmarkAverangeStd = []
+        self.numOfIslands = numOfIslands
+
+        for _ in range(0, numOfIslands):
+            self.benchmarkAverangeMax.append([])
+            self.benchmarkAverangeMin.append([])
+            self.benchmarkAverangeAvg.append([])
+            self.benchmarkAverangeStd.append([])
+
+    def setIslandData(self, min, avg, std, max, islandNumber):
+
+        self.benchmarkAverangeMax[islandNumber].append(max)
+        self.benchmarkAverangeMin[islandNumber].append(min)
+        self.benchmarkAverangeAvg[islandNumber].append(avg)
+        self.benchmarkAverangeStd[islandNumber].append(std)
+
+    def calcAvgs(self):
+        self.islands = []
+
+        for island in range(0, self.numOfIslands):
+
+            avgMax = [sum(x)/len(x)
+                      for x in zip(*self.benchmarkAverangeMax[island])]
+
+            avgAvg = [sum(x)/len(x)
+                      for x in zip(*self.benchmarkAverangeAvg[island])]
+
+            avgStd = [sum(x)/len(x)
+                      for x in zip(*self.benchmarkAverangeStd[island])]
+
+            avgMin = [sum(x)/len(x)
+                      for x in zip(*self.benchmarkAverangeMin[island])]
+
+            self.islands.append(IslandResults(
+                avgMin, avgAvg, avgStd, avgMax, island))
+
+
+benchmarkName = "sel_himmelblau.py"
+FREQ = 50
+ISLANDS = 10
+NGEN = 20000
+numOfIterations = 1
+
+pickleIn = open("./out/" + benchmarkName + "_" + str(FREQ) + "_" + str(ISLANDS) +
+                "_" + str(NGEN) + "_" + str(numOfIterations) + ".pickle", "rb")
+
+
+benchmarkResults = pickle.load(pickleIn)
+
+pickleIn.close()
+
+logbooks = benchmarkResults[0].logbooks
+hallOfFamers = benchmarkResults[0].hallOfFamers
+time = benchmarkResults[0].time
+
+islands_max = []
+islands_min = []
+islands_avg = []
+islands_std = []
+
+fig, axs = plt.subplots(int(ISLANDS / 2), 2)
+
+benchmark = Benchmark(ISLANDS)
+
+
+for benchmarkResult in benchmarkResults:
+
+    for islandNumber, logbook in enumerate(logbooks):
+
+        gen = logbook.select("gen")
+        fit_mins = logbook.select("min")
+        fit_avgs = logbook.select("avg")
+        fit_stds = logbook.select("std")
+        fit_maxs = logbook.select("max")
+
+        # islands_max.append(max(fit_maxs))
+        # islands_min.append(min(fit_mins))
+        # islands_avg.append(statistics.mean(fit_avgs))
+        # islands_avg.append(statistics.mean(fit_stds))
+
+        benchmark.setIslandData(
+            fit_mins, fit_avgs, fit_stds, fit_maxs, islandNumber)
+
+benchmark.calcAvgs()
+
+islandsAvgMax = benchmark.avgMax
+islandsAvgAvg = benchmark.avgAvg
+islandsAvgStd = benchmark.avgStd
+islandsAvgMin = benchmark.avgMin
+
+# Wykres liniowy
+
+'''
+    x = int(islandNumber / 2)
+    y = int(islandNumber % 2)
+
+    line1 = axs[x, y].plot([_ for _ in range(0, len(fit_mins))],
+                           fit_mins, "b-", label="Minimum Fitness")
+    # line2 = axs[x, y].plot([_ for _ in range(0, len(fit_mins))],
+    #                       fit_avgs, "r-", label="Average Fitness")
+    axs[x, y].set_xlabel("Generation")
+    axs[x, y].set_yscale('log')
+    axs[x, y].set_ylabel("Fitness", color="b")
+    axs[x, y].set_title("ISLAND " + str(islandNumber))
+    for tl in axs[x, y].get_yticklabels():
+        tl.set_color("b")
+
+    lns = line1  # + line2
+    labs = [l.get_label() for l in lns]
+    axs[x, y].legend(lns, labs, loc="center right")
+
+plt.show()
+'''
+
+# Wykres słupkowy
+
+fig, ax1 = plt.subplots()
+
+# Tylko ostatnie pokolenie
+#fit_mins = [fit_mins[-FREQ - 1], ]
+#fit_avgs = [fit_avgs[-FREQ - 1], ]
+#fit_maxs = [fit_maxs[-FREQ - 1], ]
+
+for islandNumber, fit_mins in enumerate(islandsAvgMin):
+    ind = np.arange(len(islandsAvgMin))  # the x locations for the groups
+    width = 0.3
+
+    bar1 = ax1.bar(ind - width, fit_mins, width, label="Minimum Fitness")
+    #bar2 = ax1.bar(ind, islands_avg, width, label="Average Fitness")
+    #bar3 = ax1.bar(ind + width, islands_max, width, label="Maximum Fitness")
+
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ax1.set_ylabel("Fitness")
+    ax1.set_xlabel("Islands")
+    ax1.set_title('Islands ' + benchmarkName)
+    ax1.set_xticks(ind)
+    ax1.set_yscale('log')
+    ax1.set_xticklabels(("ISLAND " + str(i) for i in range(0, ISLANDS)))
+    ax1.legend()
+
+    # bars = bar1 + bar2 + bar3
+
+    plt.show()
--- eksperymenty/sel konw jedno/sel_schwefel.py
+++ eksperymenty/sel konw jedno/sel_schwefel.py
@@ -12,30 +12,46 @@ import sys
 # argv[0] to nazwa programu - tak jest domyślnie
 # LICZBA EWOLUCJI MIĘDZY MIGRACJAMI (częstotliowść migracji) - argv[1]
 # LICZBA WYSP - argv[2]
-#
-for arg in sys.argv:
-    print(arg)
+# LICZBA EWOLUCJI - argv[3]
+# LICZBA POWTÓRZEŃ ALGORYTMU - argv[4]
 
+# Nazwa pliku - nazwa benchmarka
+benchmarkName = sys.argv[0]
+
+# jak często następuje migracja (co ile zmian całej populacji)
+FREQ = int(sys.argv[1])
+
+# Początkowa liczba wysp
+ISLANDS = int(sys.argv[2])
+
+# liczba generacji
+NGEN = int(sys.argv[3])
+
+# Liczba powtórzeń algorytmu
+numOfIterations = int(sys.argv[4])
+
+# Dla każdej wyspy
+POPULATION_SIZE = 100
 
 # Każdy osobnik - individual ma liczbę atrybutów = len(weights), jeżeli -1 - minimaliazcja, jeżeli 1 - maksymalizacja
-creator.create("FitnessMax", base.Fitness, weights=(1.0,))
-creator.create("Individual", list, fitness=creator.FitnessMax)
+creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+creator.create("Individual", list, fitness=creator.FitnessMin)
 
 toolbox = base.Toolbox()
 
+
+ATTRIBUTES = 20
 # Atrybut typu bool - 0 albo 1
-toolbox.register("attr_float", random.uniform, -100, 100)
+toolbox.register("attr_float", random.uniform, -500, 500)
 toolbox.register("individual", tools.initRepeat, creator.Individual,
-                 toolbox.attr_float, 2)
+                 toolbox.attr_float, ATTRIBUTES)
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 
-# Funkcja celu - suma wartości atrybutów
-
-benchmarkName = "h1"
+# Funkcja celu
 
 
 def evalOneMax(individual):
-    return sum(benchmarks.h1(individual)),
+    return sum(benchmarks.schwefel(individual)),
 
 
 toolbox.register("evaluate", evalOneMax)
@@ -45,8 +61,6 @@ toolbox.register("select", tools.selTournament, tournsize=3)
 
 toolbox.register("map", map)
 
-# Początkowa liczba wysp
-ISLANDS = 10
 
 # dodanie migracji - mig.migSel to funkcja migracji selekcji konwekcyjnej
 toolbox.register("migrate", mig.migSel, numOfIslands=ISLANDS)
@@ -57,27 +71,24 @@ stats.register("std", numpy.std)
 stats.register("min", numpy.min)
 stats.register("max", numpy.max)
 
-# liczba generacji, jak często następuje migracja (co ile zmian całej populacji)
-NGEN, FREQ = 200, 1
 
 CXPB, MUTPB = 0.5, 0.2
 # ngen = FREQ oznacza ile wykonań algorytmu się wykona przy jednym uruchomieniu funkcji
 toolbox.register("algorithm", algorithms.eaSimple, toolbox=toolbox,
                  stats=stats, cxpb=CXPB, mutpb=MUTPB, ngen=FREQ, verbose=False)
 
-# utworzenie populacji początkowej
-numOfIterations = 10
 benchmarkResults = []
 
-
-for _ in range(0, numOfIterations):
+for algNum in range(0, numOfIterations):
+    print("Iteracja algorytmu:", algNum + 1)
     logbooks = []
     # Zapisuje n najlepszych osobników (tutaj n = 1)
     hallOfFame = tools.HallOfFame(1)
+    bestIndividuals = []
     start_time = time.time()
-    islands = [toolbox.population(n=300) for i in range(ISLANDS)]
+    islands = [toolbox.population(n=POPULATION_SIZE) for i in range(ISLANDS)]
     for i in range(0, NGEN, FREQ):
-
+        print("Postęp iteracji:", (i / NGEN) * 100, "%")
         results = toolbox.map(toolbox.algorithm, islands)
 
         ziped = list(map(list, zip(*results)))
@@ -88,6 +99,8 @@ for _ in range(0, numOfIterations):
         for island in islands:
             hallOfFame.update(island)
 
+        bestIndividuals.append(hallOfFame)
+
         if i == 0:
             for logbook in ziped[1]:
                 logbooks.append(logbook)
@@ -96,23 +109,24 @@ for _ in range(0, numOfIterations):
                 logbooks[k] += logbook
 
         # Jeżeli wartość funkcji celu najlepszego osobnika jest optymalna to kończymy przetwarzanie
-        if(hallOfFame[0].fitness.values[0] == 100):
+        if(all(v == 0 for v in hallOfFame[0].fitness.values)):
             break
 
         toolbox.migrate(islands)
 
     # Save results
-    #benchmarkResults.append(utils.result(logbooks, hallOfFame))
+    benchmarkResults.append(utils.result(
+        logbooks, bestIndividuals, time.time() - start_time))
 
-'''
+
 # Jeden logbook to zapis z jednej wyspy
 # 0 - stan przed mutacjami i krzyżowaniem, ale po migracji z porzedniej iteracji
-for logbook in logbooks:
-    print(logbook)
+# for logbook in logbooks:
+#    print(logbook)
 
 print("Hall of fame:", hallOfFame[0], hallOfFame[0].fitness)
 
-pickleOut = open("selKonw_" + benchmarkName + ".pickle", "wb")
+pickleOut = open("./out/" + benchmarkName + "_" + str(FREQ) + "_" + str(ISLANDS) +
+                 "_" + str(NGEN) + "_" + str(numOfIterations) + ".pickle", "wb")
 pickle.dump(benchmarkResults, pickleOut)
 pickleOut.close()
-'''
